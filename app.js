010d10fcfa65c9e9529f3f62f986ac32a9ae6c61
Lutz B <bigsleep117@aol.com>
2021-04-06 20:25:54 +0200

    bcrypt

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
 const app = express();
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -15,20 +17,20 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-console.log(process.env.API_KEY);
+//console.log(process.env.API_KEY);
 //setup local database
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
 //create mongoose Schema for user credentials
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
 
 //const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 
 
 
@@ -36,50 +38,61 @@ userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["pass
 const User = new mongoose.model("User", userSchema);
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
 //take user credentials from register page and post it to your database
-app.post("/register", function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+app.post("/register", function(req, res) {
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+
+    //if the user is successfully saved and there is no error, than the "secrets" page will be rendered
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 
 
-//if the user is successfully saved and there is no error, than the "secrets" page will be rendered
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
 
 });
 
 //check if user credentials are already in database
-app.post("/login", function(req,res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
-    if(err){
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
       console.log(err);
     } else {
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          // result == true
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       }
     }
 
@@ -88,6 +101,6 @@ app.post("/login", function(req,res){
 
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
